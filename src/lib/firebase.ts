27b8765764d8271ea5b2632dd31024be96b12fa3
Vishal <vishal165610@gmail.com>
2026-08-21 12:45:50 +0530

    Commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ import {
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   // Always use the native Firebase auth domain to bypass browser cross-origin storage partitioning
-  authDomain: "edurackin.firebaseapp.com",
+  authDomain: "edurack.in",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
